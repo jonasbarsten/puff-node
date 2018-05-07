3f68d8d40d32af65c4b75d0d0a108f44346eb845
Jonas Barsten <jonasbarsten@gmail.com>
2018-05-07 16:34:50 +0200

    Testing imports

--- server.js
+++ server.js
@@ -4,32 +4,38 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var bonjour = require('bonjour')();
 var easymidi = require('easymidi');
-var osc = require('node-osc');
+// var osc = require('node-osc');
 
-var inputs = easymidi.getInputs();
-let teensy = '';
+var artnet = require('./modules/artnet.js');
 
-// Finding teensy with piezo (Fiskehest)
-for (var i = 0; i < inputs.length; i++) {
-  let value = inputs[i];
-  if (value.substring(0, 9) === 'Fiskehest') {
-    teensy = value;
-  }
-}
+const hei = artnet.hello;
 
-var input = new easymidi.Input(teensy);
-var client = new osc.Client('10.0.128.106', 8010);
+console.log(hei());
 
-// MIDI 2 OSC
-input.on('noteon', function (msg) {
-  // console.log(msg);
+// var inputs = easymidi.getInputs();
+// let teensy = '';
 
-  const velocityFloat = msg.velocity / 127;
-  const note = msg.note;
-  var msg = new osc.Message('/surfaces/Fixture 1/opacity', velocityFloat);
+// // Finding teensy with piezo (Fiskehest)
+// for (var i = 0; i < inputs.length; i++) {
+//   let value = inputs[i];
+//   if (value.substring(0, 9) === 'Fiskehest') {
+//     teensy = value;
+//   }
+// }
 
-  client.send(msg)
-});
+// var input = new easymidi.Input(teensy);
+// var client = new osc.Client('10.0.128.106', 8010);
+
+// // MIDI 2 OSC
+// input.on('noteon', function (msg) {
+//   // console.log(msg);
+
+//   const velocityFloat = msg.velocity / 127;
+//   const note = msg.note;
+//   var msg = new osc.Message('/surfaces/Fixture 1/opacity', velocityFloat);
+
+//   client.send(msg)
+// });
 
 bonjour.publish({ name: 'Puff 1', type: 'http', port: 9090 })
 
