e35b8591033403a41f648cd270af71a2c6b30997
Jonas Barsten <jonasbarsten@gmail.com>
2018-05-21 13:57:49 +0200

    Waiting for MIDI module in server.js

--- modules/midi.js
+++ modules/midi.js
@@ -21,28 +21,29 @@ setTimeout(() => {
 	if (teensy) {
 	  input = new easymidi.Input(teensy);
 	};
+
+	exports.noteListen = (callback) => {
+		if (input) {
+			return input.on('noteon', (msg) => {
+			  const velocityFloat = msg.velocity / 127;
+			  const note = msg.note;
+			  callback(note, velocityFloat);
+			});
+		} else {
+			callback('No teensy found', null);
+		}
+	};
+
+	exports.ccListen = (callback) => {
+		if (input) {
+			return input.on('cc', (msg) => {
+			  const valueFloat = msg.value / 127;
+			  const controller = msg.controller;
+			  callback(controller, valueFloat);
+			});
+		} else {
+			callback('No teensy found', null);
+		}
+	};
+
 }, 5000);
-
-exports.noteListen = (callback) => {
-	if (input) {
-		return input.on('noteon', (msg) => {
-		  const velocityFloat = msg.velocity / 127;
-		  const note = msg.note;
-		  callback(note, velocityFloat);
-		});
-	} else {
-		callback('No teensy found', null);
-	}
-};
-
-exports.ccListen = (callback) => {
-	if (input) {
-		return input.on('cc', (msg) => {
-		  const valueFloat = msg.value / 127;
-		  const controller = msg.controller;
-		  callback(controller, valueFloat);
-		});
-	} else {
-		callback('No teensy found', null);
-	}
-};
--- server.js
+++ server.js
@@ -89,31 +89,34 @@ const directionMap = {
   }
 };
 
-midi.noteListen((note, value) => {
-  console.log(note, value);
-  // Piezo
-  if (note == 0 || note == 1 || note == 2 || note == 3) {
-    osc.send(`/puff/${state.localIp}/piezo/${note}`, [
-      {
-        type: "f",
-        value: value
-      }
-    ]);
-  };
-});
+// Waiting since not waiting would cause crashes from time to time
+setTimeout(() => {
+  midi.noteListen((note, value) => {
+    console.log(note, value);
+    // Piezo
+    if (note == 0 || note == 1 || note == 2 || note == 3) {
+      osc.send(`/puff/${state.localIp}/piezo/${note}`, [
+        {
+          type: "f",
+          value: value
+        }
+      ]);
+    };
+  });
 
-midi.ccListen((controller, value) => {
-  console.log(controller, value);
-  // Compass
-  if (controller == 1) {
-    osc.send(`/puff/${state.localIp}/orientation`, [
-      {
-        type: "f",
-        value: value * 360
-      }
-    ]);
-  };
-});
+  midi.ccListen((controller, value) => {
+    console.log(controller, value);
+    // Compass
+    if (controller == 1) {
+      osc.send(`/puff/${state.localIp}/orientation`, [
+        {
+          type: "f",
+          value: value * 360
+        }
+      ]);
+    };
+  });
+}, 8000);
 
 osc.listen((message, info) => {
   state.lastOsc = {message, info};
