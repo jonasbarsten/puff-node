1be891945cac24ac908239310cf1f710d1aa85ab
Jonas Barsten <jonasbarsten@gmail.com>
2018-11-05 20:13:49 +0100

    Upping threshold for piezo to 0.35

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ setTimeout(() => {
         }
       ]);
 
-      if (piezoSum > 0.3 && state.activeLayers["1"]) {
+      if (piezoSum > 0.35 && state.activeLayers["1"]) {
         osc.sendLocal(`/puff/${state.localIp}/lights/layer/1/master`, [
           {
             type: "f",
